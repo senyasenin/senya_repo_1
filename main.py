11060e4749fc995a5fd3a4b31c5bd235244151d5
Senya Senin <servash77@gmail.com>
2026-04-27 23:06:26 +0300

    Senya добавил в ветку работу с DB

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+
+import pymysql
+from urllib3 import connection_from_url
+
+
 def read_data_file(file_name):
     file_lines = []
     file = open(file_name,'r')
@@ -8,11 +13,18 @@ def split_line(line):
     splited_line = line.split(';')
     return splited_line
 
+def connect_to_db(host,user,port,db_name,password):
+    connection = pymysql.connect(host=host,user=user,port=port,db=db_name,password=password)
+    cursor=connection.cursor()
+    return cursor,connection
+
+
 if __name__ == '__main__':
+    cursor, connection = connect_to_db('192.168.1.7','student',3306,'test','123456')
+    connection.commit()
     stroki_iz_faila = read_data_file('data.csv')
-    #print(stroki_iz_faila)
+
     for stroka in stroki_iz_faila:
         razbitay_stroka = split_line(stroka)
-        print(razbitay_stroka)
-        print(f'Мое имя {razbitay_stroka[0]} {razbitay_stroka[1]}, мой пол - {razbitay_stroka[2]}, год моего рождения {razbitay_stroka[3]}-й, мой контактный телефон - {razbitay_stroka[4]}')
-
+        cursor.execute("insert into sprav values('{}', '{}', '{}', '{}', '{}');".format(razbitay_stroka[0], razbitay_stroka[1],razbitay_stroka[2],razbitay_stroka[3],razbitay_stroka[4].strip()))
+    connection.commit()
